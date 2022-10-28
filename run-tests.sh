66f8c9eb82c7202d409ffafedbaa93661949831e
Juuso Issakainen <7767433+juusoi@users.noreply.github.com>
2022-10-28 16:54:49 +0300

    improve bash script

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+cd "$(dirname "$0")"
 
 docker build -t rfbrowser .
 docker run -t --rm -v "$(pwd)"/tests:/robot/tests -v "$(pwd)"/reports:/robot/reports rfbrowser --outputdir /robot/reports /robot/tests
